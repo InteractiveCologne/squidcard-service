88c6a9b1417285125c858578b5f465fd9aa7c1b9
maxim <maxim.kuepper@gmail.com>
2013-05-31 22:39:33 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquidcardService::Application.config.secret_token = 'a0046f16abde898f599abe39639e8aec8a8444ac56b02002c17712561502bcea28e7e7f82022599a977186a838f35fdd4cbae5199bfd575f9f91fcb2142f026b'
